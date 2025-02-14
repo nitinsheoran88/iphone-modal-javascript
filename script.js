62ef520c269389bb26689fcdb439bd1382495d73
nitinsheoran88 <nitinsheoran47@gmail.com>
2025-02-15 00:06:53 +0530

    project-updated

---
+++ script.js
@@ -0,0 +1,9 @@
+let poppop = document.querySelector('.modal-header');
+
+function open_box2() {
+    poppop.style.display = 'flex';
+}
+
+function close_box2() {
+    poppop.style.display = 'none';
+}
